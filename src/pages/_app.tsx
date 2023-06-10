d70186f39336d14a119028adc7b340a24374f4f2
shahab-ramzan <shahab.ramzan@codedistrict.com>
2023-06-10 18:44:20 +0500

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import { ReactElement } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface AppProps {
+  Component: React.ComponentType<any>;
+  pageProps: {
+    session: any;
+    [key: string]: any;
+  };
+}
+
+export default function App({ Component, pageProps }: AppProps): ReactElement {
+  const { session, ...restPageProps } = pageProps;
+
+  return (
+    <SessionProvider session={session}>
+      <Component {...restPageProps} />
+    </SessionProvider>
+  );
 }
