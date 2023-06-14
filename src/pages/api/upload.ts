e30b20c4d240006bcf2ad3d058366ddd5f077549
shahab Ramzan <96526562+shahab-ramzan@users.noreply.github.com>
2023-06-14 15:40:00 +0500

    upload file

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,6 +1,11 @@
+/* eslint-disable */
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import multiparty from "multiparty";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import fs from "fs";
+import mime from "mime-types";
+
 type Data = {};
 
 export default async function handler(
@@ -8,14 +13,39 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   const form = new multiparty.Form();
+  const bucketName: string = "next-ecommerce-admin";
   const { fields, files } = await new Promise((resolve, reject) => {
     form.parse(req, (err: any, fields: any, files: string | any[]) => {
       if (err) reject(err);
       resolve({ fields, files });
     });
   });
-  console.log("object", files.file.length);
-  res.status(200).json("OK");
+
+  const client = new S3Client({
+    region: "ap-southeast-1",
+    credentials: {
+      accessKeyId: process.env.S3_S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  });
+  const links = [];
+  for (const file of files.file) {
+    const ext = file.originalFilename.split(".").pop();
+    const newFilename = Date.now() + "." + ext;
+    await client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: newFilename,
+        Body: fs.readFileSync(file.path),
+        ACL: "public-read",
+        ContentType: mime.lookup(file.path),
+      })
+    );
+    const link = `https://${bucketName}.s3.amazonaws.com/${newFilename}`;
+    links.push(link);
+  }
+
+  res.status(200).json({ links });
 }
 
 export const config = {
